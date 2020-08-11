c16a4a9411bbf9b6bd4b067473dfb42cf8c81385
rost <iskandarovrost@gmail.com>
2020-08-11 18:56:28 +0400

    30.create-db.sql fixed

--- modules/core/db/init/hsql/30.create-db.sql
+++ modules/core/db/init/hsql/30.create-db.sql
@@ -1,11 +1,11 @@
 insert into SAMPLE_PRODUCT
-(ID, PROD_CODE, NAME, DESCRIPTION, PRICE)
-values ('prod2t1', 'prod2t1', 'another product of type2', 'some good product', 123.00);
+(PROD_CODE, NAME, DESCRIPTION, PRICE)
+values ( 'prod2t1', 'another product of type2', 'some good product', 123.00);
 
 insert into SAMPLE_PRODUCT
-(ID, PROD_CODE, NAME, DESCRIPTION, PRICE)
-values ('prod1t2', 'prod1t2', 'Product of type 2', 'very good product', 6.78);
+(PROD_CODE, NAME, DESCRIPTION, PRICE)
+values ('prod1t2', 'Product of type 2', 'very good product', 6.78);
 
 insert into SAMPLE_PRODUCT
-(ID, PROD_CODE, NAME, DESCRIPTION, PRICE)
-values ('prod1t1', 'prod1t1', 'product of type 1', null, 12.50);
+(PROD_CODE, NAME, DESCRIPTION, PRICE)
+values ( 'prod1t1', 'product of type 1', null, 12.50);
